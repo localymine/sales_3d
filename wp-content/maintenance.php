14c6f48c003ac1d10d38332a366e956228b9aca6
khang.ld <khang.ld@aris-vn.com>
2016-08-18 18:21:28 +0700

    add maintenance

--- wp-content/maintenance.php
+++ wp-content/maintenance.php
@@ -1,61 +1,39 @@
+<link href="http://fonts.googleapis.com/earlyaccess/notosanskannada.css" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.countdown/2.1.0/jquery.countdown.min.js"></script>
 
 <style>
-    body { text-align: center; padding: 100px; }
-    h1 { font-size: 50px; text-transform: uppercase;}
+    body { background: #f1f1f1; text-align: center; padding: 100px; font-family: 'Noto Sans Kannada', sans-serif;}
+    h1 { font-size: 25px; text-transform: uppercase; padding: 10px 10px 10px 36px; color: #555; background: #fff8c4; border-radius: 10px; border-color: #f2c779;}
     body { font: 20px Helvetica, sans-serif; color: #333; }
     article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-    a { color: #dc8100; text-decoration: none; }
-    a:hover { color: #333; text-decoration: none; }
-
-    ul#clock {
-        list-style: none;
-        margin: 50px 0;
-        padding: 0;
-        display: block;
-        text-align: center;
+    p{
+        font-weight: bold;
+        font-size: 30px;
     }
-
-    ul#clock li { display: inline-block; }
-
-    ul#clock li span {
-        font-size: 80px;
-        font-weight: 300;
-        line-height: 80px;
+    p.vn{
+        color: #FFE620;
     }
-
-    ul#clock li.seperator {
-        font-size: 80px;
-        line-height: 70px;
-        vertical-align: top;
+    p.cn{
+        color: red;
     }
-
-    ul#clock li p {
-        color: #a7abb1;
-        font-size: 25px;
+    p.en{
+        color: #006699;
     }
 </style>
 
 <article>
-    <h1>Launching Soon!</h1>
-    <div>
-        <p>&mdash; The On My Way &mdash;</p>
-    </div>
+    <h1>Warnning!</h1>
+    <p class="vn">
+        Công ty dầu ăn Honoroad chậm thanh toán tiền thiết kế website<br/>
+        Website tạm ngưng hoạt động
+    </p>
+    <p class="cn">
+        Honoroad 石油公司还没付款网站的设计<br/>
+		网站停活动
+    </p>
+    <p class="en">
+        Honoroad oil company delays payment website design<br/>
+        Website inactivity.
+    </p>
 </article>
-
-<ul id="clock"></ul>
-
-<script type="text/javascript">
-    $('#clock').countdown('2016/10/31', function (event) {
-        $(this).html(event.strftime(''
-                + '<li><span class="days">%D</span><p class="days_text">Days</p></li>'
-                + '<li class="seperator">:</li>'
-                + '<li><span class="hours">%H</span><p class="hours_text">Hours</p></li>'
-                + '<li class="seperator">:</li>'
-                + '<li><span class="minutes">%M</span><p class="minutes_text">Minutes</p></li>'
-                + '<li class="seperator">:</li>'
-                + '<li><span class="seconds">%S</span><p class="seconds_text">Seconds</p></li>'
-                ));
-    });
-</script>
